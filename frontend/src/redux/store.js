30eed8299d9d78e05a2b050aed29eda09f808473
CristianF09 <cris7i_laurentiu@yahoo.com>
2025-02-19 12:30:47 +0200

    Update file Frontend

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { loaderReducer } from './loader/loaderSlice';
+
+export const store = configureStore({
+  reducer: {
+    loader: loaderReducer,
+    // other reducers
+  },
+});
